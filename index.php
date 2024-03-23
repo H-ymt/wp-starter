878f1c31c5b8d1846d747796c261b3798044291f
H-ymt <y.handai1272@gmail.com>
2024-03-23 13:36:03 +0900

    feat(index.php): Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 <div class="grid place-items-center py-10 mx-3">
-	<h1 class="text-3xl font-bold">WordPress開発のスターターテンプレート</h1>
+	<h1 class="text-3xl font-bold">WordPress Starter template</h1>
 </div>
 <?php get_footer(); ?>
